1a26192e9c8a61317a4d52cdfac5235e7a0ec9ba
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2022-12-08 17:09:29 -0300

    Essa classe representa os itens do jogo

---
+++ PacMan/src/Item.java
@@ -0,0 +1,20 @@
+public class Item extends GameObject {
+
+	private boolean visivel;
+
+	public Item() {
+	}
+
+	public Item(int posicaoX, int posicaoY) {
+		super(posicaoX, posicaoY);
+	}
+
+	public boolean isVisivel() {
+		return visivel;
+	}
+
+	public void setVisivel(boolean visivel) {
+		this.visivel = visivel;
+	}
+
+}
